c1989083a03f808f36b730b95e54adf99e7f3eca
John Appleseed <email@example.com>
2017-11-24 18:19:19 +0530

    initial commit.. search option attached

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Posts;
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $posts=Posts::orderBy('id','DESC')->paginate(5);
+        return view('posts.index',compact('posts'));//->with('i',($request->input('page',1)-1)*5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    return view('posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    $this->validate($request,['title'=>'required','description'=>'required','user_id'=>'required']);
+    //Posts::create($request->all());
+    $post = new Posts();
+    $post->title = $request->title;
+    $post->description= $request->description;
+    $post-> user_id   = $request->user_id;
+    $post->save();
+
+    return redirect()->route('posts.index')->with('success','Post Created Successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+     $posts =Posts::findOrFail($id);
+     return view('posts.show',compact('posts'));
+    }
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+    $posts =Posts::findOrFail($id);
+    return view('posts.edit',compact('posts'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $this->validate($request,['title'=>'required','description'=>'required']);
+        Posts::findOrFail($id)->update($request->all());
+        return redirect()->route('posts.index')->with('success','Post Created successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+     Posts::findOrFail($id)->delete();
+     return redirect()->route('posts.index')->with('success','Post Deleted Successfully');
+
+    }
+
+
+    public function search(Request $request)
+    {
+     return $request;
+    }
+}
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,116 @@
+@extends('layouts.default')
+
+
+
+@section('content')
+
+
+    {!! Form::open(array('route' => 'posts.search','method'=>'POST')) !!}
+
+    <div class="col-xs-12 col-sm-12 col-md-12">
+
+        <div class="form-group">
+
+            <strong>Search for user</strong>
+
+            <td> {!! Form::text('title', null, array('placeholder' => 'Title','class' => 'form-control')) !!} </td>
+
+        </div>
+
+    </div>
+
+    <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+
+    </div>
+
+
+    {!! Form::close() !!}
+
+
+
+    <div class="row">
+
+        <div class="col-lg-12 margin-tb">
+
+            <div class="pull-left">
+
+                <h2>Posts Crud</h2>
+
+            </div>
+
+            <div class="pull-right">
+
+                <a class="btn btn-success" href="{{ route('posts.create') }}"> Create New Post</a>
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+    @if ($message = Session::get('success'))
+
+        <div class="alert alert-success">
+
+            <p>{{ $message }}</p>
+
+        </div>
+
+    @endif
+
+
+    <table class="table table-bordered">
+
+        <tr>
+
+            <th>No</th>
+
+            <th>Title</th>
+
+            <th>Description</th>
+
+            <th>User_id</th>
+            <th width="280px">Action</th>
+
+        </tr>
+                <?php $i =1; ?>
+        @foreach($posts as $item)
+
+            <tr>
+
+                <td>{{ $i++ }}</td>
+
+                <td>{{ $item->title }}</td>
+
+                <td>{{ $item->description }}</td>
+
+                <td>{{$item->user_id}}</td>
+
+                <td>
+
+                    <a class="btn btn-info" href="{{ route('posts.show',$item->id) }}">Show</a>
+
+                    <a class="btn btn-primary" href="{{ route('posts.edit',$item->id) }}">Edit</a>
+
+                    {!! Form::open(['method' => 'DELETE','route' => ['posts.destroy', $item->id],'style'=>'display:inline']) !!}
+
+                    {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
+
+                    {!! Form::close() !!}
+
+                </td>
+
+            </tr>
+
+        @endforeach
+
+    </table>
+
+
+    {!! $posts->render() !!}
+
+
+@endsection
